16290c70b946935e5db3ba2807db2c300965636c
suprachakra <111553143+suprachakra@users.noreply.github.com>
2025-02-15 19:10:47 +0800

    Create ComplianceApplication.java

---
+++ backend/services/compliance/src/main/java/com/rideshare/compliance/ComplianceApplication.java
@@ -0,0 +1,10 @@
+package com.rideshare.compliance;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplianceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ComplianceApplication.class, args);
+    }
+}
