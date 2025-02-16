7710a02c4f57de96e67c0862c0bf682b36c3162d
suprachakra <111553143+suprachakra@users.noreply.github.com>
2025-02-16 21:39:47 +0800

    Create BaseActivity.kt

---
+++ mobile/rider-android/src/main/java/com/rideshare/rider/common/BaseActivity.kt
@@ -0,0 +1,11 @@
+package com.rideshare.rider.common
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * BaseActivity provides common functionality (e.g., logging, error handling)
+ * that can be extended by all activities.
+ */
+open class BaseActivity : AppCompatActivity() {
+    // Common functions, such as showing a progress spinner or error dialogs, can be added here.
+}
