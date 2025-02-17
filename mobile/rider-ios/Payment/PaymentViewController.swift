67906043b21fd127dd19c7e278c7b8ea45edaa30
suprachakra <111553143+suprachakra@users.noreply.github.com>
2025-02-17 23:14:42 +0800

    Delete mobile/rider-ios/Payment directory

--- mobile/rider-ios/Payment/PaymentViewController.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-class PaymentViewController: BaseViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.title = "Payment"
-        // TODO: Implement payment processing UI and integrate with backend APIs.
-    }
-}
