851b86503477733d95422ce36b83999698f71362
suprachakra <111553143+suprachakra@users.noreply.github.com>
2025-02-17 23:16:12 +0800

    Create UserViewController.swift

---
+++ mobile/rider-ios/Sources/User/UserViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class UserViewController: BaseViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "My Profile"
+        // TODO: Display and manage user account details.
+    }
+}
