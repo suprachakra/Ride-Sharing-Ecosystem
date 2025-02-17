44ca54d7e6a4d88ff6aebb7fd7683db2718ad5ec
suprachakra <111553143+suprachakra@users.noreply.github.com>
2025-02-17 23:12:12 +0800

    Create BookingViewController.swift

---
+++ mobile/rider-ios/Sources/Booking/BookingViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class BookingViewController: BaseViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Book a Ride"
+        // Setup UI elements (buttons, text fields) for booking
+        // TODO: Add booking functionality
+    }
+}
