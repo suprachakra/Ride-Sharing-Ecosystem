3dfe8e524d0f252ce33fb287f26c7ebcee47aead
suprachakra <111553143+suprachakra@users.noreply.github.com>
2025-02-17 23:13:54 +0800

    Create PaymentViewController.swift

---
+++ mobile/rider-ios/Payment/PaymentViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class PaymentViewController: BaseViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Payment"
+        // TODO: Implement payment processing UI and integrate with backend APIs.
+    }
+}
